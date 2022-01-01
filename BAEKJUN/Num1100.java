c5cd1aff407d76b8cfee1ef6a11a471e1e314dc5
JaehoonShin2 <96221394+JaehoonShin2@users.noreply.github.com>
2022-01-01 23:08:11 +0900

    Update Num1100.java

--- BAEKJUN/Num1100.java
+++ BAEKJUN/Num1100.java
@@ -5,7 +5,7 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Number1100 {
+public class Num1100 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s = br.readLine();
