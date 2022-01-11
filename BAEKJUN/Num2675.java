7d42116c2e5b50030df033120bea22687b54fe85
JaehoonShin2 <96221394+JaehoonShin2@users.noreply.github.com>
2022-01-11 17:50:25 +0900

    Add files via upload

--- BAEKJUN/Num2675.java
+++ BAEKJUN/Num2675.java
@@ -8,21 +8,19 @@ import java.util.StringTokenizer;
 public class Num2675 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int textCase = Integer.parseInt(br.readLine()); // 테스트케이스
-        String[] arr = new String[textCase];
-        int val = 0;
-        for (int i = 0; i < textCase; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine()," ");
-            String row = st.nextToken();
-            val = Integer.parseInt(row);
-            arr[i] = st.nextToken();
-            String s = arr[i];
-
-            for(int j = 0; j<s.length(); j++ ){
-                for (int k = 0; k < val; k++) {
-                    System.out.print(s.charAt(j));
+        StringTokenizer st;
+        int T = Integer.parseInt(br.readLine()); // 테스트케이스
+        for (int i = 0; i < T; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int n = Integer.parseInt(st.nextToken());
+            char[] val = st.nextToken().toCharArray();
+            int length = val.length;
+            for(int j = 0; j<length; j++ ){
+                for (int k = 0; k < n; k++) {
+                    System.out.print(val[j]);
                 }
             }
+            System.out.println();
         }
     }
 }
