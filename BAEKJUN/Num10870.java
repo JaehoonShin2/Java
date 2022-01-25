a8e5db628eb0d1958b130ead72b909124dedbe62
JaehoonShin2 <96221394+JaehoonShin2@users.noreply.github.com>
2022-01-25 22:38:07 +0900

    Add files via upload

---
+++ BAEKJUN/Num10870.java
@@ -0,0 +1,26 @@
+package com.github.BAEKJUN;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Num10870 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        System.out.println(fibonacci(N));
+
+    }
+
+    public static int fibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+
+    }
+
+}
