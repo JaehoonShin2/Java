db3954331916286ac9ca2c8d6ec6282fcfc4a5f5
JaehoonShin2 <96221394+JaehoonShin2@users.noreply.github.com>
2022-02-08 22:38:40 +0900

    Add files via upload

--- BAEKJUN/Num2798.java
+++ BAEKJUN/Num2798.java
@@ -1,48 +1,52 @@
-package com.github.BAEKJUN;
+package com.kh.variable;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.IOException;
 import java.util.StringTokenizer;
 
+
 public class Num2798 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int N = Integer.parseInt(st.nextToken());
-        // ì¹´ë“œì˜ ê°¯ìˆ˜
-        int M = Integer.parseInt(st.nextToken());
-        // í•©
-        int[] arr = new int[N];
 
-        st = new StringTokenizer(br.readLine(), " ");
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-        System.out.println(search(arr, N, M));
+		public static void main(String[] args) throws IOException {
+	 
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+	        int N = Integer.parseInt(st.nextToken());
+	        // Ä«µåÀÇ °¹¼ö
+	        int M = Integer.parseInt(st.nextToken());
+	        // ÇÕ
+	        int[] arr = new int[N];
 
-    }
-    static int search(int[] arr, int n, int m) {
-        int result = 0;
-        // 3ì¤‘ forë¬¸
-        for (int i = 0; i < n - 2; i++) {
+	        st = new StringTokenizer(br.readLine(), " ");
+	        for (int i = 0; i < N; i++) {
+	            arr[i] = Integer.parseInt(st.nextToken());
+	        }
+	        int result = search(arr, N, M);
+	        System.out.println(result);
 
-            for (int j = i + 1; j < n - 1; j++) {
+	    }
+	    static int search(int[] arr, int n, int m) {
+	        int result = 0;
+	        // 3Áß for¹®
+	        for (int i = 0; i < n - 2; i++) {
 
-                for (int k = i + 2; k < n; k++) {
+	            for (int j = i + 1; j < n - 1; j++) {
 
-                    int sum = arr[i] + arr[j] + arr[k];
+	                for (int k = i + 2; k < n; k++) {
 
-                    if (sum == m) {
-                        return sum;
-                    }
-                    if (result < sum && sum < m) {
-                        result = sum;
-                    }
+	                    int sum = arr[i] + arr[j] + arr[k];
 
-                }
-            }
-        }
-        return result;
-    }
-}
+	                    if (sum == m) {
+	                        return sum;
+	                    }
+	                    if (result < sum && sum < m) {
+	                        result = sum;
+	                    }
+
+	                }
+	            }
+	        }
+	        return result;
+	    }
+	}
