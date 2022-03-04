15ee018626499571d12b08dc8e80d9b9eb487e85
JaehoonShin2 <96221394+JaehoonShin2@users.noreply.github.com>
2022-03-04 22:46:12 +0900

    Update and rename NUm2294.java to Num2294.java

--- algorithm/Num2294.java
+++ algorithm/Num2294.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class NUm2294 {
+public class Num2294 {
 
     static int[] dp;
     public static void main(String[] args) throws IOException {
